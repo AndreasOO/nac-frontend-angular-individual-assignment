81f44f113e3739727148bbc298018c98df100ed7
Andreas Ohlander <andreas.ohlander@gmail.com>
2025-03-12 13:36:35 +0100

    added navbar and products view with mockdata

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {NavbarModule} from './navbar/navbar.module';
+import {ProductsModule} from './products/products.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import {NavbarModule} from './navbar/navbar.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NavbarModule
+    NavbarModule,
+    ProductsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

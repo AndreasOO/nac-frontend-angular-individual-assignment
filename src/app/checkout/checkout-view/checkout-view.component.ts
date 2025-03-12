c0dec0994b748e678b99c7cad60293ce9106f5f8
Andreas Ohlander <andreas.ohlander@gmail.com>
2025-03-12 21:13:21 +0100

    Added boilerplate component for checkout

---
+++ src/app/checkout/checkout-view/checkout-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checkout-view',
+  standalone: false,
+  templateUrl: './checkout-view.component.html',
+  styleUrl: './checkout-view.component.css'
+})
+export class CheckoutViewComponent {
+
+}
---
+++ src/app/checkout/checkout.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CheckoutViewComponent } from './checkout-view/checkout-view.component';
+
+
+
+@NgModule({
+  declarations: [
+    CheckoutViewComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CheckoutModule { }
